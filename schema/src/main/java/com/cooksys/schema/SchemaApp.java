13cbf4ecaf754d373e9c877ef7daa37e9a73131c
GD-Coder <grd8198@gmail.com>
2017-06-23 09:11:34 -0500

    Implemented createPerson()

--- schema/src/main/java/com/cooksys/schema/SchemaApp.java
+++ schema/src/main/java/com/cooksys/schema/SchemaApp.java
@@ -7,8 +7,20 @@ public class SchemaApp {
 	
 	
 	public static void main(String[] args) {
+		Person human = new Person();
 		PersonDaoImpl person = new PersonDaoImpl();
-		person.findById(1);
+		person.findById(5);
+		human.setFirstName("James");
+		human.setLastName("Dean");
+		human.setLocation(2);
+		human.setAge(Long.valueOf(55));
+		
+		if(person.create(human)) {
+			System.out.println("Inserted");
+		}
+		else {
+			System.out.println("Something stupid happened...");
+		}
 	}
 	
 	
--- schema/src/main/java/com/cooksys/schema/person/PersonDaoImpl.java
+++ schema/src/main/java/com/cooksys/schema/person/PersonDaoImpl.java
@@ -8,11 +8,14 @@ import java.util.List;
 import com.cooksys.schema.interfacer.PersonDAO;
 
 public class PersonDaoImpl implements PersonDAO {
+	
 	JDBCDriver drive = new JDBCDriver();
 	ArrayList<Person> people = new ArrayList<Person>();
+	
+	
 	@Override
 	public List<Person> findById(Integer id) {
-		String selectTableSQL = "SELECT person_id, f_name, l_name, location FROM schema_design.\"Person\" WHERE person_id = "+ id +";";
+		String selectTableSQL = "SELECT person_id, f_name, l_name, location, age FROM schema_design.\"Person\" WHERE person_id = "+ id +";";
 		try {
 			Statement statement = drive.getConnection().createStatement();
 			ResultSet rs = statement.executeQuery(selectTableSQL);
@@ -23,8 +26,10 @@ public class PersonDaoImpl implements PersonDAO {
 				String username = rs.getString("f_name");
 				String lname = rs.getString("l_name");
 				String location = rs.getString("location");
-				System.out.println(userid + " " + username + " " + lname + " " + location);
+				String age = rs.getString("age");
+				System.out.println(userid + " " + username + " " + lname + " " + location + " " + age);
 			}
+			rs.close();
 		} catch (Exception e) {
 			System.out.println(e.toString());
 
@@ -34,12 +39,27 @@ public class PersonDaoImpl implements PersonDAO {
 
 	@Override
 	public boolean create(Person person) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public static void main(String[] args) {
+		String fn = person.getFirstName();
+		String ln = person.getLastName();
+		Integer loc = person.getLocation();
+		Long age = person.getAge();
 		
-	}
+		String insertTableSQL = "INSERT INTO schema_design.\"Person\"(person_id, f_name, l_name, location, age) "				
+				+ "VALUES('"+ fn +"', '"+ ln +"', "+ loc +", "+ age  +");";
+		try {
+			Statement statement = drive.getConnection().createStatement();			
+			statement.executeUpdate(insertTableSQL);
+				return true;
 
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			return false;
+
+		}
+
+		
+//	}
+//		System.out.println(insertTableSQL);
+//		return true;
+	}	
 }
