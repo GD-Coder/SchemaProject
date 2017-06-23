3caebb8873a0349c7c15beb5c69ae05f58798756
GD-Coder <grd8198@gmail.com>
2017-06-22 20:13:05 -0500

    Working on DAO Implementation

--- schema/src/test/java/com/cooksys/schema/SchemaAppTests.java
+++ schema/src/test/java/com/cooksys/schema/SchemaAppTests.java
@@ -1,4 +1,4 @@
-package com.cooksys.friendlr;
+package com.cooksys.schema;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FriendlrApplicationTests {
+public class SchemaAppTests {
 
 	@Test
 	public void contextLoads() {
